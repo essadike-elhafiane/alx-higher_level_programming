5a121d7bce020533f90bf41e57d8dd16eba41deb
Essadike elhafiane <saddik.bo@gmail.com>
2023-06-10 22:07:19 +0100

    Sat Jun 10 22:07:19 +01 2023

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 97 <= ord(char) and 122 >= ord(char):
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
+    print()
