e0c6a515a2b6ec6efc80fa43b942070009d051fa
Essadike elhafiane <saddik.bo@gmail.com>
2023-06-10 02:01:50 +0100

    Sat Jun 10 02:01:50 +01 2023

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for i in range(0, 99):
     if i < 10:
         print("0{:d}, ".format(i), end='')
     else:
-        print("{:d}, ".format(i), end='')
+        print(f"{i}, ", end='')
 print(i + 1)
