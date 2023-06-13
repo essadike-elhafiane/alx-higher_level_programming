3a9b5dcdc9cd743796cacceb9a93ecf0523bf2f0
Essadike elhafiane <saddik.bo@gmail.com>
2023-06-13 19:44:44 +0100

    Tue Jun 13 19:44:44 +01 2023

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if (idx > len(my_list)):
+    if (idx >= len(my_list)):
         return None
     print("{:d}".format(my_list[idx]))
